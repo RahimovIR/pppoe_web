261ae722f036299c762899e2891c01301c103a62
RahimovIR <rahimovir@gmail.com>
2016-06-04 17:42:54 +0500

    Initial app setup.

---
+++ README.md
@@ -0,0 +1,27 @@
+webpy-skeleton
+==============
+
+Skeleton for a web.py application using twitter bootstrap.
+
+Usage
+==============
+    mkdir my_sweet_app
+    cd my_sweet_app
+    git clone https://github.com/jzellman/webpy-skeleton.git .
+    rm -rf .git
+    git init .
+    git add . 
+    git commit -m 'Initial app setup.'
+
+Initial Configuration
+==============
+    pip install -r requirements.txt
+
+Running the web server 
+====
+    python app.py
+
+Running the tests
+====
+    python test.py
+
---
+++ app.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+
+import web
+
+import config
+import model as m
+
+
+VERSION = "0.0.1"
+
+urls = (
+    r'/', 'Index',
+    )
+
+app = web.application(urls, globals())
+
+# Allow session to be reloadable in development mode.
+if web.config.get('_session') is None:
+    session = web.session.Session(app, web.session.DiskStore('sessions'),
+                                  initializer={'flash': defaultdict(list)})
+
+    web.config._session = session
+else:
+    session = web.config._session
+
+
+def flash(group, message):
+    session.flash[group].append(message)
+
+
+def flash_messages(group=None):
+    if not hasattr(web.ctx, 'flash'):
+        web.ctx.flash = session.flash
+        session.flash = defaultdict(list)
+    if group:
+        return web.ctx.flash.get(group, [])
+    else:
+        return web.ctx.flash
+
+render = web.template.render('templates/',
+                             base='base',
+                             cache=config.cache)
+t_globals = web.template.Template.globals
+t_globals['datestr'] = web.datestr
+t_globals['app_version'] = lambda: VERSION + ' - ' + config.env
+t_globals['flash_messages'] = flash_messages
+t_globals['render'] = lambda t, *args: render._template(t)(*args)
+
+
+class Index:
+    def GET(self):
+        flash("success", """Welcome! Application code lives in app.py,
+        models in model.py, tests in test.py, and seed data in seed.py.""")
+        return render.index()
+
+
+# Set a custom internal error message
+def internalerror():
+    msg = """
+    An internal server error occurred. Please try your request again by
+    hitting back on your web browser. You can also <a href="/"> go back
+     to the main page.</a>
+    """
+    return web.internalerror(msg)
+
+
+# Setup the application's error handler
+app.internalerror = web.debugerror if web.config.debug else internalerror
+
+if config.email_errors.to_address:
+    app.internalerror = web.emailerrors(config.email_errors.to_address,
+                                        app.internalerror,
+                                        config.email_errors.from_address)
+
+
+# Adds a wsgi callable for uwsgi
+application = app.wsgifunc()
+if __name__ == "__main__":
+    app.run()
