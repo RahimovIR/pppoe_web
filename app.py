5d52af4346daa57c738cbf4216bc14a437f6587e
RahimovIR <rahimovir@gmail.com>
2016-06-05 23:08:52 +0500

    v0.1

--- app.py
+++ app.py
@@ -3,56 +3,69 @@ from collections import defaultdict
 import web
 
 import config
-import model as m
+from marshmallow_jsonapi import Schema, fields
+from pyroute2 import IPRoute
+from subprocess import call
+import json
 
 
 VERSION = "0.0.1"
 
+INTERFACE = "ppp0"
+
 urls = (
     r'/', 'Index',
+    r'/get_status', 'Status',
+    r'/get_log', 'Log',
+    r'/set_connect', 'Connect',
+    r'/set_disconnect', 'Disconnect',
     )
 
 app = web.application(urls, globals())
 
-# Allow session to be reloadable in development mode.
-if web.config.get('_session') is None:
-    session = web.session.Session(app, web.session.DiskStore('sessions'),
-                                  initializer={'flash': defaultdict(list)})
-
-    web.config._session = session
-else:
-    session = web.config._session
-
-
-def flash(group, message):
-    session.flash[group].append(message)
-
-
-def flash_messages(group=None):
-    if not hasattr(web.ctx, 'flash'):
-        web.ctx.flash = session.flash
-        session.flash = defaultdict(list)
-    if group:
-        return web.ctx.flash.get(group, [])
-    else:
-        return web.ctx.flash
 
 render = web.template.render('templates/',
                              base='base',
                              cache=config.cache)
-t_globals = web.template.Template.globals
-t_globals['datestr'] = web.datestr
-t_globals['app_version'] = lambda: VERSION + ' - ' + config.env
-t_globals['flash_messages'] = flash_messages
-t_globals['render'] = lambda t, *args: render._template(t)(*args)
+# t_globals = web.template.Template.globals
+# t_globals['datestr'] = web.datestr
+# t_globals['app_version'] = lambda: VERSION + ' - ' + config.env
+# t_globals['render'] = lambda t, *args: render._template(t)(*args)
 
 
 class Index:
     def GET(self):
-        flash("success", """Welcome! Application code lives in app.py,
-        models in model.py, tests in test.py, and seed data in seed.py.""")
-        return render.index()
+        return render.index(INTERFACE)
 
+class Status:
+    def GET(self):
+        st = StatusSchema()
+        web.header('Content-Type', 'application/json')
+        data, errs = st.dump(st)
+        return json.dumps(data)
+
+class Log:
+    def GET(self):
+        log = LogSchema()
+        web.header('Content-Type', 'application/json')
+        data, errs = log.dump(log)
+        return json.dumps(data)
+
+class Connect:
+    def GET(self):
+        cr = CommandResult()
+        cr.exitCode = call(["sudo", "pon", "dsl-provider"])
+        web.header('Content-Type', 'application/json')
+        data, errs = cr.dump(cr)
+        return json.dumps(data)
+
+class Disconnect:
+    def GET(self):
+        cr = CommandResult()
+        cr.exitCode = call(["sudo", "poff", "dsl-provider"])
+        web.header('Content-Type', 'application/json')
+        data, errs = cr.dump(cr)
+        return json.dumps(data)
 
 # Set a custom internal error message
 def internalerror():
@@ -64,13 +77,66 @@ def internalerror():
     return web.internalerror(msg)
 
 
+class StatusSchema(Schema):
+    id = fields.Str(dump_only=True)
+    connect = fields.Boolean()
+    details = fields.Str()
+
+    def __init__(self):
+        ip = IPRoute()
+        pppLinksIds = ip.link_lookup(ifname=INTERFACE)
+        if len(pppLinksIds) > 0:
+            state = ip.get_links(pppLinksIds)[0].get_attr('IFLA_OPERSTATE')
+            if state == 'UP':
+                self.connect = True
+                self.details = INTERFACE + " interface UP"
+            else:
+                self.connect = False
+                self.details = INTERFACE + " exist but no UP"
+            ip.close()
+        else:
+            self.connect = False
+            self.details = INTERFACE + " interface no exist"
+        super(StatusSchema, self).__init__()
+
+
+    class Meta:
+        type_ = 'pppoe_status'
+        strict = True
+
+class LogSchema(Schema):
+    id = fields.Str(dump_only=True)
+    rawLog = fields.Str()
+
+    def __init__(self):
+        self.rawLog = self.getPppoeLog()
+        super(LogSchema, self).__init__()
+
+    def getPppoeLog(self, fileName="/var/log/syslog"):
+        result = ""
+        with open(fileName) as infile:
+            for line in infile:
+                if "ppp" in line:
+                    result += line
+        return result
+    
+    
+    class Meta:
+        type_ = 'pppoe_log'
+        strict = True
+
+class CommandResult(Schema):
+    id = fields.Str(dump_only=True)
+    exitCode = fields.Int()
+    stdout = fields.Str()
+
+
+    class Meta:
+        type_ = 'pppoe_command_status'
+        strict = True
+
 # Setup the application's error handler
 app.internalerror = web.debugerror if web.config.debug else internalerror
-
-if config.email_errors.to_address:
-    app.internalerror = web.emailerrors(config.email_errors.to_address,
-                                        app.internalerror,
-                                        config.email_errors.from_address)
 
 
 # Adds a wsgi callable for uwsgi
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,41 @@
-// 
+//
+
+$( document ).ready(function() {
+    updateStatus();
+});
+
+function updateStatus() {
+    $('#status-connect').hide();
+    $('#status-disconnect').hide();
+    $.getJSON('get_status', function(result){
+        if (result.data.type == 'pppoe_status') {
+            if(result.data.attributes.connect){
+                $('#status-connect').show();
+                $('#status-disconnect').hide();
+            }else {
+                $('#status-connect').hide();
+                $('#status-disconnect').show();
+            }
+        }
+    })
+}
+
+function updateLog() {
+    $.getJSON('get_log', function(result){
+        if (result.data.type == 'pppoe_log') {
+            $('#log-container').val(result.data.attributes.rawLog);
+        }
+    })
+}
+
+function Connect() {
+    $.getJSON('set_connect', function(result){
+        console.log(result);
+    })
+}
+
+function Disconnect() {
+    $.getJSON('set_disconnect', function(result){
+        console.log(result);
+    })
+}
